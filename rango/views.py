f1fbcd98630f919a5541acca409817659f31cbe3
Zumin Li <2278100l@student.gla.ac.uk>
2021-07-26 22:33:34 +0100

    1.add about view 2.add hyperlink for about and index page 3.pass the tests_chapter3.py

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!");
+    return HttpResponse("Rango says hey there partner!<a href=\'/rango/about/\'>About</a>")
+
+# about view
+def about(request):
+    return HttpResponse("Rango says here is the about page.<a href=\'/rango/\'>Index</a>>")
+
