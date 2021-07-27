38d469f69ed9a9dae88305e8021990e319edcdc2
Zumin Li <2278100l@student.gla.ac.uk>
2021-07-27 14:36:12 +0100

    1.add static files and media file 2. update setting.py with static and media paths 3. pass the test tests_chapter4.py

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!<a href=\'/rango/about/\'>About</a>")
+    # return HttpResponse("Rango says hey there partner!<a href=\'/rango/about/\'>About</a>")
+
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    
+    return render(request,'rango/index.html',context=context_dict)
+
+
 
 # about view
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href=\'/rango/\'>Index</a>>")
+    # return HttpResponse("Rango says here is the about page.<a href=\'/rango/\'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Zumin Li'}
+
+    return render(request,'rango/about.html',context=context_dict)
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,17 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#static files path based on BASE_DIR
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [STATIC_DIR,]
+
+#templates path based on BASE_DIR
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+
+#meida path based on BASE_DIR
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -55,7 +66,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', # Check/add this line!
             ],
         },
     },
